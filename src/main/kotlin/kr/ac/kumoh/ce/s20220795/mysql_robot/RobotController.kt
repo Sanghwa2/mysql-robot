96d3aeb17ef3110ec0ba21a27050280f381c5ca4
sanghwa22 <guitar0609@naver.com>
2025-06-02 14:16:58 +0900

    create schema

---
+++ src/main/kotlin/kr/ac/kumoh/ce/s20220795/mysql_robot/RobotController.kt
@@ -0,0 +1,19 @@
+package kr.ac.kumoh.ce.s20220795.mysql_robot
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/robots")
+class RobotController(val service: RobotService) {
+//    @GetMapping("/")
+//    fun welcome() = "Welcome to Robot Server"
+
+    @GetMapping
+    fun getAllRobots(): ResponseEntity<List<Robot>> {
+        val robots = service.getAllRobots()
+        return ResponseEntity.ok(robots)
+    }
+}
---
+++ src/main/kotlin/kr/ac/kumoh/ce/s20220795/mysql_robot/RobotService.kt
@@ -0,0 +1,9 @@
+package kr.ac.kumoh.ce.s20220795.mysql_robot
+
+import org.springframework.stereotype.Service
+
+
+@Service
+class RobotService(val repository: RobotRepository){
+    fun getAllRobots(): List<Robot> = repository.findAll()
+}
